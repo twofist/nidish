a59092f5dfb792361eb0c138fa3c1b61a87acc0b
twofist <twofist@users.noreply.github.com>
2017-05-29 14:44:15 +0200

    shield pos for normal attack

--- game.js
+++ game.js
@@ -967,7 +967,23 @@ let setshieldexhaustedidle = (player, shield) =>{
 
 let setshieldnormalattack = (player, shield) =>{
 	
+	let shieldinhandy = player.y + 4; 
+	let shieldinhandx = player.x - player.width/4;
+	let perf = - 15;
 	
+	switch(player.scale.x){
+		case -playerscalew:
+				shield.scale.x = playerscalew;
+				shield.angle = 0;
+				shield.x = shieldinhandx + perf;
+				shield.y = shieldinhandy;
+			break;
+		default: 
+				shield.scale.x = -playerscalew;
+				shield.angle = -0;
+				shield.x = shieldinhandx - perf;
+				shield.y = shieldinhandy;
+	}
 
 }
 
